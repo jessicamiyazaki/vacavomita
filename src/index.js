d7f64dc9420d7c29b89a5a6b08a8b23af582bd75
jessicamiyazaki <jessicamiyazaki@gmail.com>
2019-10-17 11:01:19 -0300

    é nogocio do router hahah roteamento usando a barra / criamos o "/" e o "/transaction" e no /transaction uma forma de colocar parametros .. hj tb aprendi o treco das branches lá git e agora tem que fazer clojure. ahh e vou pull request

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
-const http = require('http');
-//liguagem orientada a call back "hell"  assincrona
+const express = require('express')
+const app = express()
+const port = 8000
 
-const server = http.createServer((req, res) => { 
+app.get('/', (req, res) =>{
+ 
     console.log(req.url, req.method) 
-    res.write("hello world")
-    res.end();
-});
+    res.send('Hello World!')
+})
 
-server.on('clientError', (err, socket) => {
-    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-});
 
-console.log ("escuando na porta 8000")
-server.listen(8000);
 
+app.get('/transaction', (req, res) =>{
+ 
+    console.log(req.url, req.method) 
+    res.send('transaction space!')
+})
+
+app.get('/transaction/:abcd', (req, res) =>{
+ 
+    console.log(req.url, req.method) 
+    res.send(`transaction ${req.params.abcd} space!`)
+})
+
+app.listen(
+    port, 
+    () => console.log(
+        `escuando na porta  ${port}!` //isso é uma interpolação de string uso esse  acento: ` e isso vai pegar a variavel e inserir dentro da str na posição marcada 
+    )
+) 
+
+
+
+// const http = require('http');
+// //liguagem orientada a call back "hell"  assincrona
+
+// const server = http.createServer((req, res) => { 
+//     console.log(req.url, req.method) 
+//     res.write("hello world")
+//     res.end();
+// });
+
+// server.on('clientError', (err, socket) => {
+//     socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+// });
+
+// console.log ("escuando na porta 8000")
+// server.listen(8000);
